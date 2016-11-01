7a3f1676aa8533dc6fc0fc04b7ff9a4a0ed91ab4
Flysha <sdflysha@qq.com>
2016-11-01 09:04:09 +0800

    Add InnerTravelVisitorTest

---
+++ sdmap/test/sdmap.test/VisitorTest/InnerTravelVisitorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace sdmap.test.VisitorTest
+{
+    public class InnerTravelVisitorTest : VisitorTestBase
+    {
+        [Fact]
+        public void CannotTravelInner()
+        {
+            var root = GetParseTree("namespace ns{sql sql{}}");
+            var visitor = new Mocks.InnerNoTravelVisitor();
+            visitor.Visit(root);
+
+            Assert.False(visitor.VisitedNamedSql);
+        }
+
+        [Fact]
+        public void CanTravelInner()
+        {
+            var root = GetParseTree("namespace ns{sql sql{}}");
+            var visitor = new Mocks.InnerTravelVisitor();
+            visitor.Visit(root);
+
+            Assert.True(visitor.VisitedNamedSql);
+        }
+    }
+}
---
+++ sdmap/test/sdmap.test/VisitorTest/Mocks/InnerTravelVisitor.cs
@@ -0,0 +1,26 @@
+﻿using sdmap.Functional;
+using sdmap.Parser.G4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Antlr4.Runtime.Misc;
+
+namespace sdmap.test.VisitorTest.Mocks
+{
+    public class InnerTravelVisitor : SdmapParserBaseVisitor<Result>
+    {
+        public bool VisitedNamedSql { get; set; } = false;
+
+        public override Result VisitNamespace([NotNull] SdmapParser.NamespaceContext context)
+        {
+            return base.VisitNamespace(context);
+        }
+
+        public override Result VisitNamedSql([NotNull] SdmapParser.NamedSqlContext context)
+        {
+            VisitedNamedSql = true;
+            return Result.Ok();
+        }
+    }
+}
