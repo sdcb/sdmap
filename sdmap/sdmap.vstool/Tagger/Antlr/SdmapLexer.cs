1287b97112840ee4737e95e7393698c0da2aa206
Flysha <sdflysha@qq.com>
2016-12-16 14:02:13 +0800

    [vstool] initial commit

---
+++ sdmap/sdmap.vstool/Tagger/Antlr/SdmapLexer.cs
@@ -0,0 +1,29 @@
+﻿using Antlr4.Runtime;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdmap.Vstool.Tagger.Antlr
+{
+    [Export(typeof(ISdmapLexer))]
+    class SdmapLexer : ISdmapLexer
+    {
+        public IEnumerable<SpannedToken> Run(IEnumerable<string> segments, int offset)
+        {
+            var lexer = new Parser.G4.SdmapLexer(new UnbufferedCharStream(new TextSegmentsCharStream(segments)));
+            while (true)
+            {
+                IToken current = lexer.NextToken();
+                if (current.Type == Parser.G4.SdmapLexer.Eof)
+                    break;
+                yield return new SpannedToken(
+                    current.Type,
+                    new Span(current.StartIndex + offset, current.StopIndex - current.StartIndex + 1));
+            }
+        }
+    }
+}
---
+++ sdmap/sdmap.vstool/Tagger/ISdmapLexer.cs
@@ -0,0 +1,16 @@
+﻿using Antlr4.Runtime;
+using Microsoft.VisualStudio.Text;
+using sdmap.Vstool.Tagger.Antlr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdmap.Vstool.Tagger
+{
+    interface ISdmapLexer
+    {
+        IEnumerable<SpannedToken> Run(IEnumerable<string> segments, int offset);
+    }
+}
